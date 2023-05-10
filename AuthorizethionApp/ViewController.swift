4180d1e7b12f3c7d17a83f1784c936b5763bab31
makssir <usikmaks3@gmail.com>
2023-05-10 22:11:07 +0200

    Initial Commit

---
+++ AuthorizethionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthorizethionApp
+//
+//  Created by Makssir on 10/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
